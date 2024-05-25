406c9494a985a8923c862dabeb1464b4471c6c8d
Michael Mwai <michaeldmwai@gmail.com>
2024-05-25 14:09:12 +0300

    Defined recurse.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Top Ten"""
+"""Recursion"""
 
 
-def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts
     of the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
                             .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "my-User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-            for child in sub_info.json().get("data").get("children")]
+    if sub_info.status_code >= 400:
+        return None
+
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
+
+    info = sub_info.json()
+    if not info.get("data").get("after"):
+        return hot_l
+
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
